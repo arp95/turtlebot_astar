ff2883d22a7f62843b90e994bb0cfb0a8a3bd6cd
Arpit Aggarwal <arpitdec5@gmail.com>
2020-04-07 10:56:02 -0400

    Updated utils.py with proper commenting of each block of code

--- src/utils.py
+++ src/utils.py
@@ -44,6 +44,12 @@ angle = 0.0
 
 # callback for subscriber
 def callback(msg):
+    """
+    Inputs:
+    
+    msg: Type Odometry. This gives us the position of the robot in the map and helps us update the position and orientation of the robot.
+    """
+    
     global x 
     global y
     global angle
@@ -53,8 +59,17 @@ def callback(msg):
     rot_q = msg.pose.pose.orientation
     (roll, pitch, angle) = euler_from_quaternion([rot_q.x, rot_q.y, rot_q.z, rot_q.w])
 
+    
 # function to set initial point
 def set_initial_point(robot_x, robot_y, robot_theta):
+    """
+    Inputs:
+    
+    robot_x: the x-coordinate of the robot location.
+    robot_y: the y-coordinate of the robot location.
+    robot_theta: the angle of the robot with respect to x-axis.
+    """
+    
     global x
     global y
     global angle
@@ -63,8 +78,17 @@ def set_initial_point(robot_x, robot_y, robot_theta):
     y = robot_y
     angle = robot_theta
     
-# function to set goal point
+    
+# function to go to goal point
 def go_to_point(robot_x, robot_y, pub_vel):
+    """
+    Inputs:
+    
+    robot_x: the x-coordinate of the robot location.
+    robot_y: the y-coordinate of the robot location.
+    pub_vel: the ROS publisher which publishes the information to topic 'cmd_vel_mux/input/navi'
+    """
+    
     global x
     global y
     global angle
@@ -95,48 +119,116 @@ def go_to_point(robot_x, robot_y, pub_vel):
         pub_vel.publish(vel_value)
         r.sleep()
 
+        
 # class for AStar
 class AStar(object):
+    
     # init function
     def __init__(self, start, goal, wheelRPM, clearance):
+        """
+        Inputs:
+        
+        start: this is the start coordinate of the robot. It is a tuple of form (x, y, theta).
+        goal: this is the goal coordinate of the robot. It is a tuple of form (x, y).
+        wheelRPM: this is the values of RPM of the wheels. It is of form (leftRPM, rightRPM).
+        clearance: this is the clearance that the robot needs to have with the obstacles.
+        """
+        
+        # start variable - tuple of of form (x, y, theta)
         self.start = start
+        
+        # goal variable - tuple of form (x, y)
         self.goal = goal
+        
+        # the map size along x and y dimensions in cms (map dimension are from -500 to 500 for both x and y direction)
         self.xLength = 500
         self.yLength = 500
+        
+        # wheelRPM variable - tuple of form (leftRPM, rightRPM)
         self.wheelRPM = wheelRPM
-        self.clearance = min(clearance + 5, 10)
+        
+        # clearance variable - distance of the robot from the obstacle
+        self.clearance = min(clearance + 5, 8)
+        
+        # radius variable - the radius of the robot (taken from turtlebot datasheet)
         self.radius = 22.0
+        
+        # wheelDistance - the distance between wheels (taken from turtlebot datasheet)
         self.wheelDistance = 29.0
+        
+        # wheelRadius - the radius of the wheels (taken from turtlebot datasheet)
         self.wheelRadius = 3.3
+        
+        # gridSize variable - custom-made variable to handle the visited nodes area
         self.gridSize = 1
+        
+        # distance - hashmap that stores net distance of the node from the start and the goal
         self.distance = {}
+        
+        # path - hashmap used for backtracking from the goal node to the start node
         self.path = {}
+        
+        # costToCome - hashmap to store the distance of the nodes from the start node
         self.costToCome = {}
+        
+        # costToGo - hashmap to store the distance of the nodes from the goal node
         self.costToGo = {}
+        
+        # visited - hashmap to store the visited nodes
         self.visited = {}
+        
+        # hashMap - custom variable
         self.hashMap = {}
         
+        # goalThreshold - threshold from goal node
+        goalThreshold = 15
+        
+        # initialise the visited array as False (meaning all nodes are not visited yet!)
         for val1 in range(-self.gridSize * self.xLength, self.gridSize * self.xLength + 1):
             for val2 in range(-self.gridSize * self.yLength, self.gridSize * self.yLength + 1):
                 self.visited[(val1, val2)] = False
     
-    # move is valid 
+    # move is valid or not
     def IsValid(self, currX, currY):
-        return (currX >= (-self.xLength + self.radius + self.clearance) and currX <= (self.xLength - self.radius - self.clearance) and currY >= (-self.yLength + self.radius + self.clearance) and currY <= (self.yLength - self.radius - self.clearance))
+        """
+        Inputs:
+        
+        currX - the current x-position of the robot.
+        currY - the current y-posiiton of the robot.
+        
+        Outputs:
+        
+        True / False depending on whether the nodes lies within the map or not.
+        """
+        
+        nodeInMap = (currX >= (-self.xLength + self.radius + self.clearance) and currX <= (self.xLength - self.radius - self.clearance) and currY >= (-self.yLength + self.radius + self.clearance) and currY <= (self.yLength - self.radius - self.clearance))
+        return nodeInMap
 
-    # checks for an obstacle
+    
+    # checks for an obstacle in the given map
     def IsObstacle(self, row, col):
+        """
+        Inputs:
+        
+        row - the current x-position of the robot.
+        col - the current y-posiiton of the robot.
+        
+        Outputs:
+        
+        True / False depending on whether the nodes lies within obstacle or not.
+        """
+        
         # constants
         sum_of_c_and_r = self.clearance + self.radius
         sqrt_of_c_and_r = 1.4142 * sum_of_c_and_r
         
-        # check circles
+        # check circles(obstacles) in the given map
         dist1 = ((row - 200.0) * (row - 200.0) + (col - 300.0) * (col - 300.0)) - ((100 + sum_of_c_and_r) * (100 + sum_of_c_and_r))
         dist2 = ((row + 200.0) * (row + 200.0) + (col - 300.0) * (col - 300.0)) - ((100 + sum_of_c_and_r) * (100 + sum_of_c_and_r))
         dist3 = ((row + 200.0) * (row + 200.0) + (col + 300.0) * (col + 300.0)) - ((100 + sum_of_c_and_r) * (100 + sum_of_c_and_r))
         dist4 = ((row) * (row) + (col) * (col)) - ((100 + sum_of_c_and_r) * (100 + sum_of_c_and_r))
         
-        # check square
+        # check first square(obstacle) in the given map
         (x1, y1) = (325 - sqrt_of_c_and_r, -75 - sqrt_of_c_and_r)
         (x2, y2) = (325 - sqrt_of_c_and_r, 75 + sqrt_of_c_and_r)
         (x3, y3) = (475 + sqrt_of_c_and_r, 75 + sqrt_of_c_and_r)
@@ -151,7 +243,7 @@ class AStar(object):
             dist5 = 0
             dist6 = 0
         
-        # check square
+        # check second square(obstacle) in the given map
         (x1, y1) = (-325 + sqrt_of_c_and_r, -75 - sqrt_of_c_and_r)
         (x2, y2) = (-325 + sqrt_of_c_and_r, 75 + sqrt_of_c_and_r)
         (x3, y3) = (-475 - sqrt_of_c_and_r, 75 + sqrt_of_c_and_r)
@@ -166,7 +258,7 @@ class AStar(object):
             dist7 = 0
             dist8 = 0
 
-        # check square
+        # check third square(obstacle) in the given map
         (x1, y1) = (125 - sqrt_of_c_and_r, -375 - sqrt_of_c_and_r)
         (x2, y2) = (125 - sqrt_of_c_and_r, -225 + sqrt_of_c_and_r)
         (x3, y3) = (275 + sqrt_of_c_and_r, -225 + sqrt_of_c_and_r)
@@ -181,12 +273,34 @@ class AStar(object):
             dist9 = 0
             dist10 = 0
         
+        # return true if obstacle, otherwise false
         if(dist1 <= 0 or dist2 <= 0 or dist3 <= 0 or dist4 <= 0 or dist5 == 0 or dist6 == 0 or dist7 == 0 or dist8 == 0 or dist9 == 0 or dist10 == 0):
             return True
         return False
     
+    
     # return updated position by taking into account non-holonomic constraint of robot
     def GetNewPositionOfRobot(self, currentNode, leftRPM, rightRPM):
+        """
+        Inputs:
+        
+        currentNode: the current node, tupe of type (x, y, theta)
+        leftRPM: the value of RPM of left wheel
+        rightRPM: the value of RPM of right wheel
+        
+        Outputs:
+        
+        x: Updated x-coordinate
+        y: Updated y-coordinate
+        theta: Updated angle value
+        cost: Net cost to move from (currentNode, (x, y))
+        dvx: linear velocity along x-direction to move from (currentNode, (x, y))
+        dvy: linear velocity along y-direction to move from (currentNode, (x, y))
+        w: angle threshold during each time step dt
+        flag: determines whether it is possible to move from (currentNode, (x, y))
+        """
+        
+        # calculate angular valocity of left wheel and right wheel and initialise vairables
         leftAngularVelocity = leftRPM * 2 * np.pi / 60.0
         rightAngularVelocity = rightRPM * 2 * np.pi / 60.0
         x = currentNode[0]
@@ -198,6 +312,8 @@ class AStar(object):
         cost = 0
         flag = True
         w = (self.wheelRadius / self.wheelDistance) * (rightAngularVelocity - leftAngularVelocity)
+        
+        # get updated node after moving 100 steps
         for index in range(0, 100):
             dvx = self.wheelRadius * 0.5 * (leftAngularVelocity + rightAngularVelocity) * math.cos(theta)
             dvy = self.wheelRadius * 0.5 * (leftAngularVelocity + rightAngularVelocity) * math.sin(theta)
@@ -211,12 +327,34 @@ class AStar(object):
             
             if(self.IsValid(x, y) == False or self.IsObstacle(x, y)):
                 flag = False
+                
+        # pruning
         if(self.hashMap.get(int(int(x * 1000) + int(y * 1))) != None):
             flag = False
+            
+        # return updated location
         return (x, y, theta, cost, dvx, dvy, w, flag)
 
+    
     # action move
     def ActionMoveRobot(self, currentNode, leftRPM, rightRPM):
+        """
+        currentNode: the current node, tupe of type (x, y, theta)
+        leftRPM: the value of RPM of left wheel
+        rightRPM: the value of RPM of right wheel
+        
+        Outputs:
+        
+        flag: True/ False, if possible to move to the updated node
+        newX: Updated x-coordinate
+        newY: Updated y-coordinate
+        newTheta: Updated angle value
+        dvx: linear velocity along x-direction to move from (currentNode, (newX, newY))
+        dvy: linear velocity along y-direction to move from (currentNode, (newX, newY))
+        dw: angle threshold during each time step dt
+        cost: Net cost to move from (currentNode, (x, y))
+        """
+        
         # update position
         (newX, newY, newTheta, cost, dvx, dvy, dw, flag) = self.GetNewPositionOfRobot(currentNode, leftRPM, rightRPM)
         self.hashMap[int(int(newX * 1000) + int(newY * 1))] = 1
@@ -226,8 +364,25 @@ class AStar(object):
             return (True, newX, newY, newTheta, dvx, dvy, dw, cost)
         return (False, newX, newY, newTheta, dvx, dvy, dw, cost)
 
+    
     # update action
     def UpdateAction(self, currentNode, weight, newX, newY, newTheta, action, cost):
+        """
+        Inputs:
+        
+        currentNode: the current node, tupe of type (x, y, theta)
+        weight: cost to move from (currentNode,(newX, newY))
+        newX: Updated x-coordinate
+        newY: Updated y-coordinate
+        newTheta: Updated angle value
+        action: containing dvx and dvy values for further storing
+        cost: the additional cost to move from (currentNode, (newX, newY))
+        
+        Outputs:
+        
+        True / False depedning on whether an optimal node found
+        """
+        
         newCostToCome = self.costToCome[currentNode] + weight
         newCostToGo = self.euc_heuristic(newX, newY)
         newDistance = newCostToCome + newCostToGo + cost
@@ -242,12 +397,35 @@ class AStar(object):
             return True
         return False
         
+        
     # eucledian heuristic (becomes weighted a-star when weight made greater than 1.0)
-    def euc_heuristic(self, currX, currY, weight = 3.0):
+    def euc_heuristic(self, currX, currY, weight = 1.0):
+        """
+        Inputs:
+        
+        currX - the current x-position of the robot.
+        currY - the current y-posiiton of the robot.
+        weight - the weight used for A-star algorithm
+        
+        Output:
+        
+        Returns the eucledian distance between goal node and the current node(currX, currY)
+        """
+        
         return weight * (np.sqrt(self.gridSize * ((self.goal[0] - currX) ** 2) + ((self.goal[1] - currY) ** 2)))
+    
     
     # a-star algo
     def search(self):
+        """
+        Outputs:
+        
+        exploredStates: the states explored when moving from start node to goal node.
+        backtrackStates: the path from start node to goal node.
+        actions: list containing the (dvx, dvy) values for each possible node between start and goal node.
+        distance: the total distance between start node and goal node.
+        """
+        
         # mark source node and create a queue
         exploredStates = []
         queue = []
@@ -259,8 +437,9 @@ class AStar(object):
         flag = 0
         steps = 0
         
-        # run a-star
+        # run A-star
         while(len(queue) > 0):
+            
             # get current node
             _, _, currentNode = heappop(queue)
             self.visited[(int(round(self.gridSize * currentNode[0])), int(round(self.gridSize * currentNode[1])))] = True   
@@ -269,13 +448,13 @@ class AStar(object):
             steps = steps + 1
             
             # if goal node then break, using the distance formula
-            if(np.square(np.abs(currentNode[0] - self.goal[0])) + np.square(np.abs(currentNode[1] - self.goal[1])) < 15):
+            if(np.square(np.abs(currentNode[0] - self.goal[0])) + np.square(np.abs(currentNode[1] - self.goal[1])) < self.goalThreshold):
                 backtrackNode = currentNode
                 flag = 1
                 break
                
-            # break if steps greater than 5000000
-            if(steps > 500000):
+            # break if steps greater than 1000000 (exit when no path exists)
+            if(steps > 1000000):
                 break
 
             # traverse the edges
